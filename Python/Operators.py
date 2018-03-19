cd08d88f8de64eb8e192c944679e77fbc28c9046
Mark Nay <crazydude547@gmail.com>
2018-03-18 22:19:53 -0400

    Organization

---
+++ Python/Operators.py
@@ -0,0 +1,18 @@
+print(2+4)
+print(3-5)
+print(2- -5)
+print(3*8)
+print(2**4)
+
+
+
+print(7/3)
+print(7.0/3)
+print (7 // 3)
+#Double Slash Forces Integer Division
+
+print(5/0)
+#Cannot Divide By Zero
+
+print( 8 % 5)
+#Percent Sign Gives Remainders
---
+++ Python/Strings.py
@@ -0,0 +1,15 @@
+science = "SCIENCE"
+apple = "apples"
+
+print(apple)
+apple = apple.upper()
+#makes "apple" uppercase
+print(apple)
+
+print(science.lower())
+print(science.title())
+
+apple = "            apple              "
+apple = apple.strip().upper()
+print(apple)
+#Strip takes away white space
